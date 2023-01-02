66e20f891c4b283c4a3aaf259578de5cc00a2921
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2023-01-03 00:21:45 +0300

    Add TaskFragment

---
+++ to_do/app/src/main/java/com/src/todo/TaskFragment.kt
@@ -0,0 +1,17 @@
+package com.src.todo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TaskFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task, container, false)
+    }
+}
